b36f7c9d6cfc5772b218dd099a90727bb5b9230d
Laurita <Laurita@imp-jacorp>
2019-11-17 01:45:10 -0600

    *  Se modifico la clase Ticket para que la tabla en base de datos concuerde con la estructura definida

--- src/main/java/gt/com/plicod/tickets/model/Ticket.java
+++ src/main/java/gt/com/plicod/tickets/model/Ticket.java
@@ -6,26 +6,49 @@
 package gt.com.plicod.tickets.model;
 
 import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
  * @author Laurita
  */
 @Entity
+@Table (name="at_ticket")
 public class Ticket {
-    @Id 
+    @Id
+    @Column (name="ticket_id", nullable=false)
+    @NotNull
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     Long ticketId;
+    @Column (name="asunto", nullable=false, length=50)
+    @NotNull
+    @Size (max=50)
     String asunto;
+    @Column (name="descripcion", nullable=false, length=500)
+    @NotNull
+    @Size (max=500)
     String descripcion;
+    @Column (name="estado", nullable=false, length=1)
+    @NotNull
+    @Size (max=1)
     String estado;
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal (TemporalType.TIMESTAMP)
+    @Column (name="fecha_creacion", nullable=false)
+    @NotNull
     Date fechaCreacion;
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal (TemporalType.TIMESTAMP)
+    @Column (name="fecha_solucion")
     Date fechaSolucion;
+    @Column (name="tecnico_id")
     Integer tecnicoId;
 
     public Ticket() {
