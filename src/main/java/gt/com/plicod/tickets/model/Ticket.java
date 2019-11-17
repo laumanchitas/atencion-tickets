f22121e8070697048523aa4602eed3b0a4d865bc
Laurita <Laurita@imp-jacorp>
2019-11-17 01:08:09 -0600

    *  Se creo clase ticket (clase de entidad)

---
+++ src/main/java/gt/com/plicod/tickets/model/Ticket.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.com.plicod.tickets.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Laurita
+ */
+@Entity
+public class Ticket {
+    @Id 
+    Long ticketId;
+    String asunto;
+    String descripcion;
+    String estado;
+    @Temporal(TemporalType.TIMESTAMP)
+    Date fechaCreacion;
+    @Temporal(TemporalType.TIMESTAMP)
+    Date fechaSolucion;
+    Integer tecnicoId;
+
+    public Ticket() {
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaSolucion() {
+        return fechaSolucion;
+    }
+
+    public void setFechaSolucion(Date fechaSolucion) {
+        this.fechaSolucion = fechaSolucion;
+    }
+
+    public Integer getTecnicoId() {
+        return tecnicoId;
+    }
+
+    public void setTecnicoId(Integer tecnicoId) {
+        this.tecnicoId = tecnicoId;
+    }
+
+    
+}
